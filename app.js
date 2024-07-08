4fec0b05f01dd0cfb1dd94184c2786c1e92a6e2b
CeliaYeung <celia5354@gmail.com>
2024-07-08 18:33:02 +0200

    Added images and grid

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 /* Navbar response burger menu toggle */
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById('navbar')
+  if (x.className === 'navbar') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navbar'
   }
+}
+
+// window.onload = function () {
+//   createImageGrid()
+// }
+
+// function createImageGrid() {
+//   var folder = 'images/gallery'
+//   $.ajax({
+//     url: folder,
+//     dataType: "jsonp",
+//     success: function (data) {
+//       $(data)
+//         .find('a')
+//         .attr('href', function (i, val) {
+//           if (val.match(/\.(jpe?g|png|gif)$/)) {
+//             $('.wrapper').append("<img src='" + folder + val + "'>")
+//           }
+//         })
+//     },
+//   })
+// }
