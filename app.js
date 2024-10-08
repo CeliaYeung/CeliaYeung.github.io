cf399bdd9538b194a5fb622ce028c9ff1ba97ba5
CeliaYeung <celia5354@gmail.com>
2024-10-08 20:21:23 +0200

    Added image modal

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ function myFunction() {
 // }
 
 window.onload = function () {
-  var lazyloadImages = document.querySelectorAll("img.lazy");
+
+  /* Lazy load images */
+  let lazyloadImages = document.querySelectorAll("img.lazy");
   console.log("Lazy load images " + lazyloadImages.length);
 
   if ("IntersectionObserver" in window) {
@@ -45,9 +47,32 @@ window.onload = function () {
       });
     });
 
-    lazyloadImages.forEach(function(lazyLoadImage) {
+    lazyloadImages.forEach(function (lazyLoadImage) {
       lazyImageObserver.observe(lazyLoadImage);
     });
+  }
+
+
+  /* Zoom in on images */
+  let modal = document.getElementById("imageDisplayModal");
+  const gridImages = document.querySelectorAll('.grid-image');
+
+  gridImages.forEach(gridImage => {
+    gridImage.addEventListener('click', function () {
+      // Your click event code here
+      console.log('Grid image clicked:', this);
+      loadImageModal(this.querySelector('img'))
+    });
+  });
+
+  let closeBtn = document.getElementsByClassName("close-btn")[0];
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+
+    /* Undide body scroll bar when modal is not showing */
+    let bodyElement = document.body;
+    bodyElement.style.overflowY = 'auto';
   }
 }
 
@@ -74,5 +99,36 @@ function openIllustrationCategory(event, category) {
 }
 
 document.getElementById("illustrationDefaultTab").click();
+
+function generateImageID(fileName) {
+  let imageID = "";
+  for (i in fileName) {
+    if (i == 0) {
+      imageID = imageID + fileName[i];
+    }
+
+    else {
+      imageID = imageID + (fileName[i].charAt(0).toUpperCase() + fileName[i].slice(1));
+    }
+    console.log(imageID);
+  }
+
+  return imageID;
+
+}
+
+function loadImageModal(imageElement) {
+  let modal = document.getElementById("imageDisplayModal");
+  let modalImage = document.getElementById("modalImage");
+  let caption = document.getElementById("modalCaption");
+
+  modal.style.display = "block";
+  modalImage.src = imageElement.src;
+  caption.innerHTML = imageElement.alt;
+
+  /* Hide body scroll bar when modal is showing */
+  let bodyElement = document.body;
+  bodyElement.style.overflowY = 'hidden';
+}
 
 
