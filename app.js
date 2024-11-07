4cc23ca99232db06bbfb892eac9962dad0628be3
CeliaYeung <celia5354@gmail.com>
2024-11-07 21:30:53 +0200

    Code clean up and added responsiveness for other pages

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-/* Navbar response burger menu toggle */
-// function myFunction() {
-//   var x = document.getElementById('navbar')
-//   if (x.className === 'navbar') {
-//     x.className += ' responsive'
-//   } else {
-//     x.className = 'navbar'
-//   }
-// }
-
-// window.onload = function () {
-//   createImageGrid()
-// }
-
-// function createImageGrid() {
-//   var folder = 'images/gallery'
-//   $.ajax({
-//     url: folder,
-//     dataType: "jsonp",
-//     success: function (data) {
-//       $(data)
-//         .find('a')
-//         .attr('href', function (i, val) {
-//           if (val.match(/\.(jpe?g|png|gif)$/)) {
-//             $('.wrapper').append("<img src='" + folder + val + "'>")
-//           }
-//         })
-//     },
-//   })
-// }
-
-
-
 // Responsive side navbar
 function showSideNavbar() {
   document.getElementById("side-navbar").style.width = "250px";
